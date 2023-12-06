8b82bb8adf0b3666fbbf93b5d415c3769b6a6d17
dariysthefirst <114654512+dariysthefirst@users.noreply.github.com>
2023-12-06 17:48:23 +0200

    Update 1task.cpp

--- 1task.cpp
+++ 1task.cpp
@@ -12,10 +12,10 @@ int main() {
   int totalMin;
   int newHours;
   int newMin;
-  
+
   cout << "enter time of the start: ";
   cin >> time.hours >> time.minutes;
-  
+
   if(time.hours > 24 || time.minutes > 60){
     cout << "invalid input.";
     return 0;
@@ -26,9 +26,10 @@ int main() {
   totalMin = time.hours * 60 + time.minutes + timeAfter.minutes;
   newHours = (totalMin / 60) % 24;
   newMin = totalMin % 60;
-
+  
   if(newMin < 10)
-    cout << "the time is: " << newHours << ":0" << newMin;
+    cout << "the time is: " << newHours << ":0";
   else
-    cout << "the time is: " << newHours << ":" << newMin;
+    cout << "the time is: " << newHours << ":";
+  cout << newMin;
 }
